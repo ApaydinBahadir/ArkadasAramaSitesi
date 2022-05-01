ef359a64ccd8bff510cf3a94a7769890ede608ed
EnginKisin <101111960+EnginKisin@users.noreply.github.com>
2022-05-01 07:12:38 +0300

    Add files via upload

---
+++ java/web/AAS/Member.java
@@ -0,0 +1,20 @@
+package web.AAS;
+
+public class Member {
+	public int userID;
+	public String username;
+	public String email;
+	public String registerDate;
+	public String password_;
+	public String birthDate;
+	public String gender;
+	public String privelege;
+	public void setusername(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setemail(String email2) {
+		// TODO Auto-generated method stub
+		
+	}
+}
